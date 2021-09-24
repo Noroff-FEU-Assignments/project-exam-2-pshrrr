fd14874e83364719aaf9185b88944fa7c21696f5
pshrrr <erevensen@gmail.com>
2021-09-24 19:14:23 +0200

    FREDAG 2000

--- pages/projects.js
+++ pages/projects.js
@@ -2,46 +2,63 @@ import Meta from "../components/layout/Head";
 import Layout from "../components/layout/Layout";
 import Image from "next/image";
 import Link from "next/link";
-import { BASE_URL } from "../components/constants/api";
+import React, { useEffect, useState } from "react";
 
-const Projects = ({ projects }) => {
-  return (
-    <Layout>
-      <Meta title="OUR PROJECTS" description="Showcasing our projects" />
-      <h1 className="regh1">PROJECTS</h1>
+function Projects() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-      <div className="strek-2"></div>
+  useEffect(() => {
+    fetch("https://sheltered-earth-62986.herokuapp.com/projects")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+          console.log(result);
+        },
 
-      <div className="flex-2">
-        {projects.map((project) => (
-          <div className="card" key={project.id}>
-            <div className="card-text">
-              <h4>{project.name}</h4>
-              <p>{project.work}</p>
-              <img
-                src={project.img_url}
-                alt="Company Picture"
-                width="150px"
-              ></img>
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Layout>
+        <Meta title="OUR PROJECTS" description="Showcasing our projects" />
+        <h1 className="regh1">PROJECTS</h1>
+
+        <div className="strek-2"></div>
+
+        <div className="flex-2">
+          {items.map((project) => (
+            <div className="card" key={project.id}>
+              <div className="card-text">
+                <h4>{project.name}</h4>
+                <p>{project.work}</p>
+                <img
+                  src={project.img_url}
+                  alt="Company Picture"
+                  width="150px"
+                ></img>
+              </div>
+
+              <Link href="/details/[id]" as={`/details/${project.id}`}>
+                <button className="btn">Read more</button>
+              </Link>
             </div>
-
-            <Link href="/details/[id]" as={`/details/${project.id}`}>
-              <button className="btn">Read more</button>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </Layout>
-  );
-};
-
-export const getStaticProps = async () => {
-  const res = await fetch(BASE_URL);
-  const data = await res.json();
-
-  return {
-    props: { projects: data },
-  };
-};
+          ))}
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default Projects;
